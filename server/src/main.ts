9852633e46304e575d25f3206c6ac3ef0911491b
niv626 <nive626@gmail.com>
2023-05-07 17:15:19 +0300

    change endpoint cors in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors({
+    credentials: true,
+    origin: 'https://note-me-server.onrender.com:3000',
+  });
   // app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
