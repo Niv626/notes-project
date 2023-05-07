3d8fdea97f36e5562ea1d709537698e9a183b05d
niv626 <nive626@gmail.com>
2023-05-07 17:24:55 +0300

    change endpoint

--- client/src/api/authApi.tsx
+++ client/src/api/authApi.tsx
@@ -13,7 +13,7 @@ export const login = async ({ email, password, firstName, lastName }) => {
 };
 
 export const app = axios.create({
-  baseURL: "https://note-me-server.onrender.com:3333",
+  baseURL: "https://note-me-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
