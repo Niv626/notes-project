2e23cf5264776a8eadb235dcde26990d2f5a51f3
niv626 <nive626@gmail.com>
2023-05-11 17:17:57 +0300

    css stuff

--- server/dist/main.js
+++ server/dist/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
         credentials: true,
-        origin: 'https://note-me.onrender.com',
+        origin: 'http://localhost:3000',
     });
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
